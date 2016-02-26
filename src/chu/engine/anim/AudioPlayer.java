7a1817465c6fe9dc50816034455665ad2380fa5b
Raymond Dodge <git@rayrobdod.name>
2016-02-25 19:37:00 -0500

    Remove unused camera methods in AudioPlayer

--- src/chu/engine/anim/AudioPlayer.java
+++ src/chu/engine/anim/AudioPlayer.java
@@ -11,18 +11,6 @@ import chu.engine.Game;
  * The Class AudioPlayer.
  */
 public class AudioPlayer {
-
-	/** The camera. */
-	static Camera camera;
-
-	/**
-	 * Sets the camera.
-	 *
-	 * @param c the new camera
-	 */
-	public static void setCamera(Camera c) {
-		camera = c;
-	}
 	
 	/**
 	 * Play audio.
